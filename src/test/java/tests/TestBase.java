e6205790e6d00b3cff4207c9671d1eef52724aca
Ahmed Taha <a.taha@Billqode.local>
2025-09-04 14:23:54 +0300

    OK

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,32 +2,29 @@ package tests;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
 public class TestBase {
     public static WebDriver driver;
-    public String generalEmail = "ahmed@gmai485555545444l858.com";
-    public String generalPassword = "123456_Test";
 
+    public String generalEmail = "ahmed@gmai4854555545444l858.com";
+    public String generalPassword = "123456_Test";
 
     @BeforeSuite
 
     public void Start() {
         ChromeOptions options = new ChromeOptions();
-
-
+        options.addArguments("--headless=new");  // أو "--headless"
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver();
-        options.addArguments("--headless");
+        driver.manage().window().maximize();
         driver.get("https://demo.nopcommerce.com/");
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(webDriver -> ((JavascriptExecutor) webDriver)
