8199139a4c760da640c7f0de143de04d13934ab6
Ahmed Taha <a.taha@Billqode.local>
2025-09-04 19:02:00 +0300

    last

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.xml.xpath.XPath;
+import java.time.Duration;
 
 public class HomePage extends PageBase{
     public HomePage(WebDriver driver) {
@@ -17,13 +20,23 @@ public class HomePage extends PageBase{
     @FindBy(css = ".ico-login")
     WebElement login;
 
-    public void clickRegister(){
-        waitForInvisibility(By.cssSelector(".ico-register']"));
-        clickBtn(registerBtn);
+//    public void clickRegister(){
+//        waitForInvisibility(By.cssSelector(".ico-register']"));
+//        clickBtn(registerBtn);
+//
+//    }
+public void clickRegister() {
+    // Wait for the register button to be clickable before clicking
+    // The locator used in the wait should be a By object
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    wait.until(ExpectedConditions.elementToBeClickable(registerBtn));
+    clickBtn(registerBtn);
+}
 
-    }
 
     public void loginFromHome(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(login));
         clickBtn(login);
     }
 }
