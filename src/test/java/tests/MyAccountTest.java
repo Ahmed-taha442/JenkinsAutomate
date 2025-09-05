0a7a36558c4f02328e2ff76860867741dafb1512
Ahmed Taha <a.taha@Billqode.local>
2025-09-05 17:10:11 +0300

    update in my account

--- src/test/java/tests/MyAccountTest.java
+++ src/test/java/tests/MyAccountTest.java
@@ -17,32 +17,43 @@ public class MyAccountTest extends TestBase{
     public void goMyAccount(){
         myAccountPage=new MyAccountPage(driver);
         myAccountPage.myAccount();
-
-    }
-    @Test(priority = 2)
-    public void changePassword(){
         myAccountPage.changPassword(generalPassword,"123456","123456");
         Assert.assertEquals(myAccountPage.assertChangePassword.getText(),"Password was changed");
-    }
-    @Test(priority = 3)
-    public void closeAlert(){
         myAccountPage.closeAlert();
-    }
-    @Test(priority = 4)
-    public void  logout (){
         myAccountPage.logOut();
-    }
-    @Test(priority = 5)
-    public void login(){
         homePage=new HomePage(driver);
         homePage.loginFromHome();
-    }
-
-    @Test(priority = 6)
-    public void validLogin(){
         loginPage = new LoginPage(driver);
         loginPage.login(generalEmail,"123456");
         loginPage.submitBtn();
 
+
+
     }
+//    @Test(priority = 2)
+//    public void changePassword(){
+//        myAccountPage.changPassword(generalPassword,"123456","123456");
+//        Assert.assertEquals(myAccountPage.assertChangePassword.getText(),"Password was changed");
+//    }
+//    @Test(priority = 3)
+//    public void closeAlert(){
+//        myAccountPage.closeAlert();
+//    }
+//    @Test(priority = 4)
+//    public void  logout (){
+//        myAccountPage.logOut();
+//    }
+//    @Test(priority = 5)
+//    public void login(){
+//        homePage=new HomePage(driver);
+//        homePage.loginFromHome();
+//    }
+//
+//    @Test(priority = 6)
+//    public void validLogin(){
+//        loginPage = new LoginPage(driver);
+//        loginPage.login(generalEmail,"123456");
+//        loginPage.submitBtn();
+//
+//    }
 }
